d06c45e0ca25093df71e70327f5bab6a18eaa736
Luizgcsneto <luizgonzaga15@gmail.com>
2021-05-11 23:49:32 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace avaliacao_global
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int numero = 20, i = 20;
+            while (i != 1)
+            {
+                for (i = 20; i >= 2; i--)
+                {
+                    if (numero % i != 0)
+                        break;
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine($"O número {numero} é o que estamos procurando.");
+                }
+                else
+                    numero += 20;
+            }
+        }
+    }
+}
