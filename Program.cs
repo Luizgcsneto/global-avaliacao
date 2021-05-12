f5fb48623dd40f2d85553729adbb242615b28f76
Luizgcsneto <luizgonzaga15@gmail.com>
2021-05-11 23:54:48 -0300

    os dois exemplos agora de 1 a 10 e 1 a 20

--- Program.cs
+++ Program.cs
@@ -6,15 +6,34 @@ namespace avaliacao_global
     {
         static void Main(string[] args)
         {
-             int numero = 20, i = 20;
+
+            int numeroUM = 10, i = 10;
             while (i != 1)
             {
-                for (i = 20; i >= 2; i--)
+                for (i = 10; i >= 2; i--)
                 {
-                    if (numero % i != 0)
+                    if (numeroUM % i != 0)
                         break;
                 }
                 if (i == 1)
+                {
+                    Console.WriteLine($"O número {numeroUM} é o que estamos procurando.");
+                }
+                else
+                    numeroUM += 10;
+            }
+
+            
+            // de 1 a 20
+            int numero = 20, j = 20;
+            while (j != 1)
+            {
+                for (j = 20; j >= 2; j--)
+                {
+                    if (numero % j != 0)
+                        break;
+                }
+                if (j == 1)
                 {
                     Console.WriteLine($"O número {numero} é o que estamos procurando.");
                 }
